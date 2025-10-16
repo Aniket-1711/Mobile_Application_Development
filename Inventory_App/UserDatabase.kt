78fa9e55964cc49253047b00a9d4546dd4b12cf6
Aniket Kauraji Gawande <gawandeaniketh@gmail.com>
2025-10-16 10:12:08 +0530

    Implement UserDatabase with Room persistence library

--- Inventory_App/UserDatabase.kt
+++ Inventory_App/UserDatabase.kt
@@ -1,1 +1,29 @@
+package com.example.inventoryapp
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.Room
+
+@Database(entities = [User::class], version = 1)
+abstract class UserDatabase: RoomDatabase(){
+    abstract fun userDao(): UserDao
+
+
+    companion object{
+        @Volatile
+        var INSTANCE:UserDatabase?=null
+
+        fun getInstance(context: Context): UserDatabase{
+            return INSTANCE?:synchronized(this){
+                val instace=Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDatabase::class.java,
+                    "aniket_db"
+                ).build()
+                INSTANCE=instace
+                instace
+            }
+        }
+    }
+}
