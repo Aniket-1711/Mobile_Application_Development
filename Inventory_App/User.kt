317d9665c62ebdf73d5b1d2aabec243fd1d16dc5
Aniket Kauraji Gawande <gawandeaniketh@gmail.com>
2025-10-16 09:45:44 +0530

    Add User data class with Room entity annotation

--- Inventory_App/User.kt
+++ Inventory_App/User.kt
@@ -1,1 +1,13 @@
 
+package com.example.inventoryapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "CSE F Users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val uid:Int,
+    val userName:String,
+    val userPhone:Int,
+)
+
